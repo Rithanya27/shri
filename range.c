856d63f6dd3b5b03a633846b972e08f654075bc3
Swetha911 <Swetha911@users.noreply.github.com>
2016-07-03 00:46:23 +0530

    Create range.c

---
+++ range.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+int i;
+printf("enter number");
+scanf("%d",&i);
+if(i==1||i==2||i==3||i==4||i==5||i==6||i==7||i==8||i==9)
+{
+printf("the number is %d",i);
+}
+else{
+printf("the number you entered is not in range");
+}
+}
