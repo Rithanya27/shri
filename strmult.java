9e4bc2621ed64d4b8d71e73df758d3be7085184b
Swetha911 <Swetha911@users.noreply.github.com>
2016-07-03 20:15:34 +0530

    Update strmult.java

--- strmult.java
+++ strmult.java
@@ -1,22 +1,30 @@
 import java.util.Scanner;
+
+
 public class strintmult {
     public static void main(String[] args)
     {
         String str1="";
         String str2="";
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter the 1st string : ");
+        System.out.println("Enter the 1st string : ");
         str1 = in.nextLine();
-        System.out.print("Enter the 2nd string : ");
+        System.out.println("Enter the 2nd string : ");
         str2 = in.nextLine();
         
         int a = Integer.parseInt(str1.trim());
         int b = Integer.parseInt(str2.trim());
-        
-        int c=a*b;
+        int c = 0;
+        if(a>0 && b>0)
+        {
+         c=a*b;
+        }
+        else{
+          System.out.println("invalid input");  
+        }
         StringBuilder sb=new StringBuilder();
         sb.append(c);
        String res=sb.toString();
-       System.out.print("result is"+res);
+       System.out.println("result is : "+res);
     }
 }
