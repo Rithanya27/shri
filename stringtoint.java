c81cf83a6f802a19ddd29c3eeb8b350c6dde8f20
Swetha911 <Swetha911@users.noreply.github.com>
2016-07-03 14:49:41 +0530

    Create stringtoint.java

---
+++ stringtoint.java
@@ -0,0 +1,23 @@
+
+public class string_int {
+   
+  public static void main (String[] args)
+  {
+  
+    String s = "100";
+
+    try
+    {
+     
+      int i = Integer.parseInt(s.trim());
+
+     
+      System.out.println("int i = " + i);
+    }
+    catch (NumberFormatException nfe)
+    {
+      System.out.println("NumberFormatException: " + nfe.getMessage());
+    }
+  }
+}
+
